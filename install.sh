da8d826862b13df5e0a593486f18ac93d25f225e
nightknight94 <xenofly94@gmail.com>
2018-12-27 09:49:49 +0100

    VS Code is now installed from repo

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgro
 gsettings set org.gnome.desktop.screensaver picture-uri file:///usr/share/backgrounds/earth.jpg
 
 # Installing packages
-packages=(git wget cmake firefox terminator)
+packages=(git wget cmake firefox terminator curl)
 
 for package in "${packages[@]}" 
 do
@@ -24,15 +24,18 @@ do
 done
 
 # Install VS Code
-# for now there is minor bug with snap version (might need manual installation from .deb)
-sudo snap install --classic vscode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code -y &>/dev/null
 
 # Install VS Code extensions
-vscode --install-extension ms-vscode.cpptools
-vscode --install-extension twxs.cmake
-vscode --install-extension EugenWiens.bitbake
-vscode --install-extension CoenraadS.bracket-pair-colorizer
-vscode --install-extension jdinhlife.gruvbox
+code --install-extension ms-vscode.cpptools
+code --install-extension twxs.cmake
+code --install-extension EugenWiens.bitbake
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension jdinhlife.gruvbox
 
 # Configure terminator
 cp -v terminator/config $HOME/.config/terminator/config
